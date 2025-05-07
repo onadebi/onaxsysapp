baee2f0f4c51f5e2868da1a516bd0522e5fcc3d8
Onaefe Edebi <edebi@onaxsys.com>
2025-05-07 13:25:14 -0400

    secured error endpoint for testing application-insights

---
+++ WebApp/Controllers/Api/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Helpers.Filters;
+
+namespace WebApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet("exception")]
+        [AuthAttributeDynamicOnx]
+        public IActionResult CreateAppException()
+        {
+            throw new Exception("This is a test exception");
+        }
+    }
+}
